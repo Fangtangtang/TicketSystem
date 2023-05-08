28a08011436d3260519d49d8d4378d73fcb2a3e2
Fangtangtang <2167295472@qq.com>
2023-05-09 00:49:50 +0800

    add Time class

--- src/utility/tool.hpp
+++ src/utility/tool.hpp
@@ -5,6 +5,7 @@
 #define TICKETSYSTEM_TOOL_HPP
 
 #include <cstring>
+#include <iostream>
 
 struct CompareUsername;
 
@@ -34,7 +35,72 @@ struct CompareUsername {
 };
 
 class Time {
-      short
+    short month = 0;
+    short date = 0;
+    short hour = 0;
+    short minute = 0;
+public:
+    Time() = default;
+
+    Time(short month_, short date_, short hour_, short minute_) : month(month_), date(date_),
+                                                                  hour(hour_), minute(minute_) {};
+
+    friend std::ostream &operator<<(std::ostream &os, const Time &time_) {
+        os << 0 << time_.month << '-';
+        if (time_.date < 9)os << 0;
+        os << time_.date << ' ';
+        if (time_.hour < 9)os << 0;
+        os << time_.hour << ':';
+        if (time_.minute < 9)os << 0;
+        os << time_.minute;
+        return os;
+    }
+
+    friend Time operator+(const Time &a, const Time &b) {
+        auto minute_ = short(a.minute + b.minute);
+        auto hour_ = short(a.hour + b.hour + minute_ / 60);
+        minute_ %= 60;
+        auto date_ = short(a.date + b.date + hour_ / 24);
+        hour_ %= 24;
+        auto month_ = short(a.month + b.month);
+        if (month_ == 6) {
+            if (date_ > 30) {
+                ++month_;
+                date_ -= 30;
+            }
+        }
+        if (month_ == 7) {
+            if (date_ > 31) {
+                ++month_;
+                date_ -= 31;
+            }
+        }
+        return {month_, date_, hour_, minute_};
+    }
+
+    /*
+     * add minute_ to time
+     * calculate from relative time to accurate time
+     */
+    void addMinute(const short &minute_) {
+        minute = short(minute_ + minute);
+        hour = short(hour + minute / 60);
+        minute %= 60;
+        date = short(date + hour / 24);
+        hour %= 24;
+        if (month == 6) {
+            if (date > 30) {
+                ++month;
+                date -= 30;
+            }
+        }
+        if (month == 7) {
+            if (date > 31) {
+                ++month;
+                date -= 31;
+            }
+        }
+    }
 };
 
 #endif //TICKETSYSTEM_TOOL_HPP
