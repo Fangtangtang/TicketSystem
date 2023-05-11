ea3307dd4337374a6bf9e2c15f800b9cf87dba42
Fangtangtang <2167295472@qq.com>
2023-05-11 22:25:10 +0800

    create Ticket class and compare classes

--- src/head-files/releasedTrain.hpp
+++ src/head-files/releasedTrain.hpp
@@ -1,4 +1,22 @@
+/*
+ * RELEASED_TRAIN SYSTEM
+ * manage information of released train
+ * deal with cmd
+ *         release_train
+ *         query_ticket
+ *         query_transfer
+ *         buy_ticket
+ *         refund_ticket
+ */
+
 #ifndef TICKETSYSTEM_RELEASEDTRAIN_HPP
 #define TICKETSYSTEM_RELEASEDTRAIN_HPP
+
+#include "../utility/bpt.hpp"
+#include "../utility/vector.hpp"
+#include "../utility/pair.hpp"
+#include "../utility/tool.hpp"
+#include "../utility/file_manager.hpp"
+#include "tokenScanner.hpp"
 
 #endif //TICKETSYSTEM_RELEASEDTRAIN_HPP
--- src/utility/tool.hpp
+++ src/utility/tool.hpp
@@ -31,6 +31,10 @@ Username::Username(char *username_) {
     strcpy(username, username_);
 }
 
+/*
+ * Compare class for Username
+ * =============================================================================
+ */
 struct CompareUsername {
     bool operator()(const Username &a, const Username &b) const {
         return strcmp(a.username, b.username) < 0;
@@ -57,6 +61,8 @@ public:
 
     explicit TrainID(char *trainID_);
 
+    friend bool operator<(const TrainID &a, const TrainID &b);
+
     friend std::ostream &operator<<(std::ostream &os, const TrainID &information);
 
 };
@@ -66,11 +72,19 @@ TrainID::TrainID(char *trainID_) {
     strcpy(trainID, trainID_);
 }
 
+bool operator<(const TrainID &a, const TrainID &b) {
+    return strcmp(a.trainID, b.trainID) < 0;
+}
+
 std::ostream &operator<<(std::ostream &os, const TrainID &information) {
     os << information.trainID;
     return os;
 }
 
+/*
+ * Compare class for TrainID
+ * =============================================================================
+ */
 struct CompareTrainID {
     bool operator()(const TrainID &a, const TrainID &b) {
         return strcmp(a.trainID, b.trainID) < 0;
@@ -94,6 +108,14 @@ public:
         --day;
         hour += day * 24;
         minutes = minute + hour * 60;
+    }
+
+    friend bool operator<(const Time &a, const Time &b) {
+        return a.minutes < b.minutes;
+    }
+
+    friend bool operator==(const Time &a, const Time &b) {
+        return a.minutes == b.minutes;
     }
 
     friend std::ostream &operator<<(std::ostream &os, const Time &time_) {
@@ -156,7 +178,7 @@ public:
         return minutes > other.minutes ? minutes - other.minutes : other.minutes - minutes;
     }
 
-    void AddMinute(int delta) {
+    void AddMinute(const int &delta) {
         minutes += delta;
     }
 
@@ -317,5 +339,90 @@ void Station::PrintInformation(const int &interval) {
  * manage basic information of ticket between two stations
  * store
  */
+class CompareTicket;
+
+class TimeBased;
+
+class CostBased;
+
+class Ticket {
+    TrainID trainID;
+    char from[30] = {'\0'};
+    char to[30] = {'\0'};
+    int price = 0;
+    //TODO 是否保留time或arriving_time
+    int time = 0;
+    Time leaving_time;
+    Time arriving_time;
+    long seat_addr = 0;
+
+    friend CompareTicket;
+    friend TimeBased;
+    friend CostBased;
+public:
+    Ticket() = default;
+
+    Ticket(const TrainID &trainID_, char *from_, char *to_,
+           const int &price_, const int &time_, const Time &leaving_time_, const Time &arriving_time_,
+           const long &seat_addr);
+
+    long PrintInformation();
+};
+
+Ticket::Ticket(const TrainID &trainID_, char *from_, char *to_, const int &price_, const int &time_,
+               const Time &leaving_time_, const Time &arriving_time_, const long &seat_addr_) :
+        trainID(trainID_), price(price_), time(time_),
+        leaving_time(leaving_time_), arriving_time(arriving_time_),
+        seat_addr(seat_addr_) {
+    memset(from, 0, sizeof(from));
+    strcpy(from, from_);
+    memset(to, 0, sizeof(to));
+    strcpy(to, to_);
+}
+
+long Ticket::PrintInformation() {
+    std::cout << trainID << ' '
+              << from << ' ' << leaving_time
+              << " -> "
+              << to << ' ' << arriving_time
+              << ' ' << price;
+    return seat_addr;
+}
+
+/*
+ * Compare class for Ticket
+ * =============================================================================
+ */
+struct CompareTicket {
+    bool operator()(const Ticket &a, const Ticket &b);
+};
+
+bool CompareTicket::operator()(const Ticket &a, const Ticket &b) {
+    int cmp = strcmp(a.from, b.from);
+    if (cmp) return cmp < 0;
+    cmp = strcmp(a.to, b.to);
+    if (cmp) return cmp < 0;
+    if (!(a.leaving_time == b.leaving_time)) return a.leaving_time < b.leaving_time;
+    if (!(a.arriving_time == b.arriving_time)) return a.arriving_time < b.arriving_time;
+    return a.trainID < b.trainID;
+}
+
+struct TimeBased {
+    bool operator()(const Ticket &a, const Ticket &b);
+};
+
+bool TimeBased::operator()(const Ticket &a, const Ticket &b) {
+    if (a.time != b.time) return a.time < b.time;
+    return a.trainID < b.trainID;
+}
+
+struct CostBased {
+    bool operator()(const Ticket &a, const Ticket &b);
+};
+
+bool CostBased::operator()(const Ticket &a, const Ticket &b) {
+    if (a.price != b.price) return a.price < b.price;
+    return a.trainID < b.trainID;
+}
 
 #endif //TICKETSYSTEM_TOOL_HPP
